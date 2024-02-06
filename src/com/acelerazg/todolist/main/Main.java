baa8fd0434da37c3ade923103b537f69dd352fa0
Geovane de Andrade <geovanedeandradegrangeiro@gmail.com>
2024-02-06 07:38:23 -0300

    Primeiro commit

---
+++ src/com/acelerazg/todolist/main/Main.java
@@ -0,0 +1,7 @@
+package com.acelerazg.todolist.main;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/acelerazg/todolist/main/utils/Tarefa.java
@@ -0,0 +1,10 @@
+package com.acelerazg.todolist.main.utils;
+
+public class Tarefa {
+    String nome;
+    Integer nivelDePrioridade;
+    String status;
+    String categoria;
+    String descricao;
+    String dataDeTermino;
+}
